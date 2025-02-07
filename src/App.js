4d5c9a4bcff3e207d7c32704ff769ebaeb6cab3d
9hkmo <gurah129@gmail.com>
2025-02-08 04:08:19 +0900

    ✅ Header, Footer, registerPage App.js에서 테스트 해보기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage/LoginPage";
-import Register from "./LoginPage/Register";
+import RegisterPage from "./pages/register/RegisterPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// 로그인 페이지는 Header, Footer를 App.js에서 사용
+// 회원가입 페이지는 Header, Footer를 페이지 내(RegisterPage)에서 사용
+// 페이지 내에서 사용했을 때 Footer를 바닥에 고정시켰음(flex를 세로로 정렬시켜 justify-content: space-between 사용)
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/" element={
+        <>
+          <Header fontSize="50px"/>
+          <LoginPage />
+          <Footer />
+        </>
+      } />
+      <Route path="/register" element={<RegisterPage />} /> 
     </Routes>
   );
 }
