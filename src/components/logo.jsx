f552a1c609127b9e1d9f25bbc9c5d4503883e8c0
9hkmo <gurah129@gmail.com>
2025-02-08 02:22:19 +0900

    🍱 mail 아이콘 추가 및 Logo 컴포넌트에서 변경

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 import lock from "../assets/icon-lock.svg";
+import mail from "../assets/icon-mail.svg";
 import { useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: center;
     gap: 10px;
     cursor: pointer;
 `;
@@ -15,7 +14,6 @@ const Img = styled.img`
 `;
 
 const Text = styled.span`
-    font-family: "Pretendard";
     font-weight: bold;
     color: ${(props) => props.color ? props.color : "black"};
     font-size: ${(props) => props.fontSize ? props.fontSize : "36px"};
@@ -32,7 +30,7 @@ function Logo(props) {
 
     return (
         <Wrapper onClick={onClick}>
-            <Img fontSize={fontSize} src={lock} />
+            <Img fontSize={fontSize} src={mail} />
             <Text fontSize={fontSize} color="#5D5A88">스팸</Text>
             <Text fontSize={fontSize} color="#8D8BA7">스캐너</Text>
         </Wrapper>
