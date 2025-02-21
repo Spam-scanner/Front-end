efda0b85210170055749d4847656c1405bf1b844
9hkmo <gurah129@gmail.com>
2025-02-21 17:42:38 +0900

    💄 로그인 유효성 검사 폼 회원가입하고 동일하게 + 로그인 성공 시 analysis 페이지로 이동

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,8 @@
-  import { useForm } from "react-hook-form";
-  import { Link } from "react-router-dom"; 
-  import styled from "styled-components";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-  const Wrapper = styled.div`
+const Wrapper = styled.div`
       display: flex;
       flex-direction: column;
       align-items: center;
@@ -10,25 +10,27 @@
       height: 680px;
   `;
 
-  const Title = styled.span`
+const Title = styled.span`
       font-size: 36px;
       font-weight: bold;
+    padding-bottom: 20px;
       color: #5D5A88;
   `;
 
-  const Form = styled.form`
+const Form = styled.form`
       display: flex;
       flex-direction: column;
       width: 380px; 
   `;
 
-  const Label = styled.label`
+const Label = styled.label`
       font-size: 14px;
-      margin-top: 20px;
+      margin-top: 15px;
       color: #8D8BA7;
   `;
 
-  const Input = styled.input`
+const Input = styled.input`
+    font-family: 'Pretendard-Regular';
       padding: 16px 20px;
       margin-top: 5px;
       font-size: 16px;
@@ -47,16 +49,18 @@
       }
   `;
 
-  const Button = styled.input`
+const Button = styled.input`
+    font-family: 'Pretendard-Regular';
       font-size: 14px;
       font-weight: 900;
       background-color: #5D5A88;
       color: white;
       border: 1px solid #5D5A88;
       border-radius: 50px;
-      margin-top: 40px;
+      margin-top: 20px;
       cursor: pointer;
       height: 50px;
+      transition: background-color 0.3s ease, opacity 0.3s ease;
 
       &:hover {
           opacity: 0.8;
@@ -66,10 +70,13 @@
           background-color: #dadada;
           border: none;
           cursor: not-allowed;
+          &:hover {
+            opacity: 1;
+          }
       }
   `;
 
-  const StyledLink = styled(Link)`
+const StyledLink = styled(Link)`
       font-size: 14px;
       font-weight: 900;
       background-color: white;
@@ -84,58 +91,74 @@
       align-items: center; 
       justify-content: center; 
       text-decoration: none;
-
   `;
 
-  const Error = styled.span`
-      color: tomato;
-      font-size: 13px;
-  `;
+const InputField = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 20px;
+    position: relative;
+`;
 
-  function Login() {
-      const {
-          register,
-          handleSubmit,
-          formState: { isValid, errors }
-      } = useForm({
-          mode: "onBlur",
-      });
+const Error = styled.span`
+    color: tomato;
+    font-size: 13px;
+    position: absolute;
+    bottom: 0px;
+    margin-top: 2px;
+`;
 
-      const onSubmit = (data) => {
-          try {
-              console.log("로그인 데이터:", data);
-              alert("로그인 성공!");
-          } catch (error) {
-              console.log(error);
-          }
-      };
+function Login() {
+    const navigate = useNavigate();
+    const {
+        register,
+        handleSubmit,
+        formState: { isValid, errors }
+    } = useForm({
+        mode: "onBlur",
+    });
 
-      return (
-          <Wrapper>
-              <Title>로그인</Title>
-              <Form onSubmit={handleSubmit(onSubmit)}>
-                  <Label>아이디</Label>
-                  <Input placeholder="아이디를 입력해주세요." type="text"
-                      {...register('id', {
-                          required: '이 필드는 필수 입력 항목입니다.',
-                      })}
-                  />
-                  {errors.id && <Error>{errors.id.message}</Error>}
+    const onSubmit = (data) => {
+        try {
+            console.log("로그인 데이터:", data);
+            alert("로그인 성공!");
+            navigate("/analysis");
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-                  <Label>비밀번호</Label>
-                  <Input placeholder="비밀번호를 입력해주세요." type="password"
-                      {...register('password', {
-                          required: '이 필드는 필수 입력 항목입니다.',
-                      })}
-                  />
-                  {errors.password && <Error>{errors.password.message}</Error>}
+    return (
+        <Wrapper>
+            <Title>로그인</Title>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <InputField>
+                    <Label>아이디</Label>
+                    <Input placeholder="아이디를 입력해주세요." type="text"
+                        {...register('id', {
+                            required: '필수 입력 사항입니다.',
+                        })}
+                    />
+                    {errors.id && <Error>{errors.id.message}</Error>}
+                </InputField>
 
-                  <Button type="submit" value="로그인" disabled={!isValid} />
+                <InputField>
+                    <Label>비밀번호</Label>
+                    <Input placeholder="비밀번호를 입력해주세요." type="password"
+                        {...register('password', {
+                            required: '필수 입력 사항입니다.',
+                        })}
+                    />
+                    {errors.password && <Error>{errors.password.message}</Error>}
+                </InputField>
 
-                  <StyledLink to="/register">회원가입</StyledLink>
-              </Form>
-          </Wrapper>
-      );
-  }
+                <Button type="submit" value="로그인" disabled={!isValid} />
 
-  export default Login;
+                <StyledLink to="/register">회원가입</StyledLink>
+            </Form>
+        </Wrapper>
+    );
+}
+
+export default Login;
